82a0a348eb01f9e31f0247f0c7a7c548542d31aa
16017004 <16017004@L815WD220022.edu.cbe.ab.ca>
2022-03-30 13:28:37 -0600

    tyert

---
+++ CS10/src/CS10.java
@@ -0,0 +1,27 @@
+/*
+
+Program: CS10.java          Date: 30-March-2022
+
+
+Author: Li Tan
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+public class CS10 {
+
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
+
+/* Screen Dump
+
+
+
+
+ */
