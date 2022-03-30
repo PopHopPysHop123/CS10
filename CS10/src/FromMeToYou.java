551b3ab60e5afdebbdcb85f56f513a8b35f442af
16017004 <16017004@L815WD220022.edu.cbe.ab.ca>
2022-03-30 13:16:13 -0600

    oiio

---
+++ CS10/src/FromMeToYou.java
@@ -0,0 +1,20 @@
+/*
+
+Program: ${name}.java          Date: ${date}
+
+
+Author: Li Tan
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+public class FromMeToYou {
+
+	public static void main(String[] args) 
+	{
+		// Li Tan
+
+	}
+
+}
