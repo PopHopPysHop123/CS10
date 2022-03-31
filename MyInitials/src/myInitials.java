7a44837d8a4587f7aaeca3258df2de76cb53386a
16017004 <16017004@L815WD220022.edu.cbe.ab.ca>
2022-03-31 15:01:10 -0600

    qwe

---
+++ MyInitials/src/myInitials.java
@@ -0,0 +1,28 @@
+/*
+
+Program: FromMeToYou.java          Date: 30-March-2022
+
+
+Author: Li Tan
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+public class
+myInitials
+{
+
+	public static void main(String[] args) 
+	{
+
+	}
+
+}
+
+/* Screen Dump
+
+
+
+
+ */
