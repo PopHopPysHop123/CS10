96b071b40a3ab38dc8ec80ad7b44f824d88ac0e7
16017004 <16017004@L815WD220007.edu.cbe.ab.ca>
2022-04-07 14:38:18 -0600

    kki

---
+++ Credit 3/src/assignments/Assignment5.java
@@ -0,0 +1,30 @@
+package assignments;
+/*
+
+Program: Assignment5.java          Date: 7-April-2022
+
+
+Author: Li Tan
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+public class Assignment5 {
+
+	public static void main(String[] args) 
+	{
+		int problem1 = 79 + 3 * (4 + 82 - 68) - 7 +19;
+		int problem2 = (179 + 21 + 10) / 7 + 181;
+		int problem3 = 10389 * 56 * 11 + 2246;
+		System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 = " + problem1);
+		System.out.println("\r(179 + 21 + 10) / 7 + 181 = " + problem2);
+		System.out.println("\r10389 * 56 * 11 + 2246 = " + problem3);
+	}
+
+}
+
+/* Screen Dump
+
+
+ */
