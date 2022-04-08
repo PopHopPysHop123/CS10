c9ffacadfe7579240062b7ea36b2764dec7d6926
16017004 <16017004@L815WD220007.edu.cbe.ab.ca>
2022-04-08 11:24:13 -0600

    2eqw

--- Credit 3/src/assignments/Assignment6.java
+++ Credit 3/src/assignments/Assignment6.java
@@ -24,11 +24,13 @@ public class Assignment6 {
 			System.out.print("What grade are you in? ");
 			int grade = input.nextInt();
 			System.out.print("");
-			String nothing = input.nextLine();
+			@SuppressWarnings("unused")
+			String unused = input.nextLine();//Prevents the code from skipping the next question
 			System.out.print("What school do you attend? ");
 			String school = input.nextLine();
 			System.out.print("What is your favourite hobby? ");
 			String hobby = input.nextLine();
+			System.out.println("Hello, your name is " + firstname + " " + lastname + ", you are currently in grade " + grade + " at " + school + ". Your favourite hobby is " + hobby + ".");
 		}
 		
 
@@ -36,6 +38,11 @@ public class Assignment6 {
 
 }
 /* Screen Dump
-
+What is your first name? Li
+What is your last name? Tan
+What grade are you in? 10
+What school do you attend? Crescent Heights HS
+What is your favourite hobby? Origami
+Hello, your name is Li Tan, you are currently in grade 10 at Crescent Heights HS. Your favourite hobby is Origami.
 
  */
