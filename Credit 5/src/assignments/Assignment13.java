b913c474cfcd5dfea2bc17a5e566f9496f3cccc5
16017004 <16017004@L815WD220007.edu.cbe.ab.ca>
2022-04-26 15:29:26 -0600

    asfasf

---
+++ Credit 5/src/assignments/Assignment13.java
@@ -0,0 +1,40 @@
+package assignments;
+/*
+
+Program: Assignment13.java          Date: 26-April-2022
+
+
+Author: Li Tan
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+import java.util.Scanner;
+
+import org.w3c.dom.ranges.Range;
+public class Assignment13
+{
+
+	public static void main(String[] args)
+	{
+		try (Scanner userinput = new Scanner(System.in))
+		{
+			System.out.print("Please enter the number of copies to be printed: ");
+			int copies = userinput.nextInt();//record the user's input
+			
+			float pricepercopy = 0;//create a double for the price per copy
+			
+			
+		}
+		
+		catch (Exception e)
+		{
+			System.out.println("ERROR");//display "ERROR" when the code doesn't work
+		}
+	}
+
+}
+/* Screen Dump
+
+ */
