c5ccdaee004749826f6f309b99c69fb9ef83b24c
16017004 <16017004@L815WD220007.edu.cbe.ab.ca>
2022-05-05 15:34:41 -0600

    asfasf

---
+++ Credit 5/src/java_project/Chem_App.java
@@ -0,0 +1,29 @@
+package java_project;
+/*
+
+Program: Chem_App.java          Date: 5-May-2022
+
+
+Author: Li Tan
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+import java.util.Scanner;
+public class Chem_App 
+{
+
+	public static void main(String[] args) 
+	{
+		try (Scanner userinput = new Scanner(System.in)) 
+		{
+			System.out.println("Welcome to the Chemistry program!\r" +
+			"Please enter the letter p for the pH and pOH converter, c for the concentration calculator, or q for practice calculation questions:");
+
+	}
+
+}
+/* Screen Dump
+
+ */
